c20bff1ce87843b020edb4ea20fff51977c1c1df
kr521 <macao450@163.com>
2020-05-27 10:30:47 +0800

    修改Dockerfile编译方式

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+export TIME_ZONE="Asia/Shanghai"
+
+sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories 
+apk --no-cache add tzdata 
+echo "${TIME_ZONE}" > /etc/timezone 
+ln -sf /usr/share/zoneinfo/${TIME_ZONE} /etc/localtime 
+mkdir /lib64
+ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
+
+
 [ "$DB_URL" ] || DB_URL='user:password@tcp(192.168.10.100:3306)/kingfisher'
 [ "$LISTEN" ] || LISTEN=0.0.0.0
 [ "$PORT" ] || PORT=8080
