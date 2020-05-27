350225593030900a9e2badbf9f77a6e8e61cd6da
kr521 <macao450@163.com>
2020-05-27 17:04:48 +0800

    修改Dockerfile编译方式

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,17 @@
 [ "$MYSQL_HOST" ] || MYSQL_HOST='mysql'
 [ "$MYSQL_PORT" ] || PORT="3306"
 [ "$MYSQL_DB" ] || DB="kingfisher"
-
 [ "$DB_URL" ] || DB_URL= ${MYSQL_USER}:${MYSQL_PASSWORD}@tcp(${MYSQL_HOST}:${MYSQL_PORT})/${MYSQL_DB}
+
+[ "$MQ_USER" ] || MQ_USER=kingfisher
+[ "$MQ_PASSWORD" ] || MQ_PASSWORD=kingfisher
+[ "$MQ_HOST" ] || MQ_HOST=king-rabbitmq
+[ "$MQ_PORT" ] || MQ_PORT=5672
+[ "$RABBITMQ_URL" ] || RABBITMQ_URL=amqp://${MQ_USER}:${MQ_PASSWORD}@${MQ_HOST}:${MQ_PORT}/
+
+
 [ "$LISTEN" ] || LISTEN=0.0.0.0
 [ "$PORT" ] || PORT=8080
-[ "$RABBITMQ_URL" ] || RABBITMQ_URL='amqp://user:password@king-rabbitmq:5672/'
 [ "$TIME_ZONE" ] || TIME_ZONE="Asia/Shanghai"
 [ "$ALPINE_REPO" ] || ALPINE_REPO="mirrors.aliyun.com"
 
