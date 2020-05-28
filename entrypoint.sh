5178d3a80fa84e02b8a9c6827a4c244998c3bc88
kr521 <macao450@163.com>
2020-05-28 16:48:09 +0800

    修复entrypoint.sh文件错误

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,13 @@
 [ "$MYSQL_HOST" ] || MYSQL_HOST='mysql'
 [ "$MYSQL_PORT" ] || PORT="3306"
 [ "$MYSQL_DB" ] || DB="kingfisher"
-[ "$DB_URL" ] || DB_URL="${MYSQL_USER}:${MYSQL_PASSWORD}@tcp\(${MYSQL_HOST}:${MYSQL_PORT}\)/${MYSQL_DB}"
+[ "$DB_URL" ] || DB_URL="${MYSQL_USER}:${MYSQL_PASSWORD}@tcp(${MYSQL_HOST}:${MYSQL_PORT})/${MYSQL_DB}"
 
 [ "$MQ_USER" ] || MQ_USER=kingfisher
 [ "$MQ_PASSWORD" ] || MQ_PASSWORD=kingfisher
 [ "$MQ_HOST" ] || MQ_HOST=king-rabbitmq
 [ "$MQ_PORT" ] || MQ_PORT=5672
-[ "$RABBITMQ_URL" ] || RABBITMQ_URL=amqp://${MQ_USER}:${MQ_PASSWORD}@${MQ_HOST}:${MQ_PORT}/
+[ "$RABBITMQ_URL" ] || RABBITMQ_URL="amqp://${MQ_USER}:${MQ_PASSWORD}@${MQ_HOST}:${MQ_PORT}/"
 
 
 [ "$LISTEN" ] || LISTEN=0.0.0.0
